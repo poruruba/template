3141e4841ea25ac6e9e15a4185e649a103c4e6d8
poruruba <44470781+poruruba@users.noreply.github.com>
2018-11-03 10:23:07 +0900

    Create echo.js

---
+++ templ_simple/api/controllers/echo.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  echo: echo,
+  test_post: test,
+  test_get: test
+};
+
+function echo(req, res) {
+    console.log("calling: echo");
+
+    res.json({ message: req.body, result: 'OK'});
+}
+
+function test(req, res) {
+  console.log("calling: test");
+
+  var event = {
+    headers: req.headers,
+    body: JSON.stringify(req.body),
+    path: req.swagger.apiPath,
+    httpMethod: req.method,
+    queryStringParameters: req.query
+  };
+
+  res.json( { event: event } );
+}
