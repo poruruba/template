74e56da52c9d8abf1f855f23b8ab82efcb458da6
poruruba <44470781+poruruba@users.noreply.github.com>
2019-02-03 23:04:25 +0900

    Update alexa-utils.js

--- templ_simple/api/helpers/alexa-utils.js
+++ templ_simple/api/helpers/alexa-utils.js
@@ -94,7 +94,7 @@ class BaseIntentHandler{
 
     handle(handlerInput) {
         console.log('handle: ' + this.matcher + ' called');
-        return this.myhandle(handlerInput);
+        return await this.myhandle(handlerInput);
     }
 }
   
