03638582a907fccd595193d10db11d0dc7e5d7be
poruruba <44470781+poruruba@users.noreply.github.com>
2019-02-03 23:03:57 +0900

    Update routing.js

--- templ_simple/api/controllers/routing.js
+++ templ_simple/api/controllers/routing.js
@@ -2,14 +2,17 @@
 
 /* 関数を以下に追加する */
 const func_table = {
-//  "test-func" : require('./test-func').handler,
-//  "test-dialogflow" : require('./test-dialogflow').fulfillment,
+//  "test-func" : require('./test_func').handler,
 };
 const alexa_table = {
-  //  "test-alexa" : require('./test-alexa-skill').handler,
+//  "test-alexa" : require('./test_alexa').handler,
 };
 const lambda_table = {
 //  "test-lambda" : require('./test-lambda').handler,
+//  "forward_lambda" : require('./forward_lambda').handler,
+};
+const express_table = {
+//    "test-cloba": require('./test-clova-skill').handler,
 };
 /* ここまで */
 
@@ -21,124 +24,162 @@ const binary_table = [
 
 var exports_list = {};
 for( var operationId in func_table ){
-  exports_list[operationId] = routing;
+    exports_list[operationId] = routing;
 }
 for( var operationId in alexa_table ){
-  exports_list[operationId] = routing;
+    exports_list[operationId] = routing;
 }
 for( var operationId in lambda_table ){
-  exports_list[operationId] = routing;
+    exports_list[operationId] = routing;
+}
+for( var operationId in express_table ){
+    exports_list[operationId] = express_table[operationId];
 }
 
 module.exports = exports_list;
 
 function routing(req, res) {
-//  console.log(req);
+//    console.log(req);
 
-  var operationId = req.swagger.operation.operationId;
-  console.log('[' + operationId + ' calling]');
+    var operationId = req.swagger.operation.operationId;
+    console.log('[' + operationId + ' calling]');
 
-  try{
-    var event;
-    var func;
-    if( func_table.hasOwnProperty(operationId) ){
-      event = {
-        headers: req.headers,
-        body: JSON.stringify(req.body),
-        path: req.swagger.apiPath,
-        httpMethod: req.method,
-        queryStringParameters: req.query,
-        requestContext: ( req.requestContext ) ? req.requestContext : {}
-      };
-    
-      if( event.headers['content-type'] )
-        event.headers['Content-Type'] = event.headers['content-type'];
-      if( event.headers['authorization'] )
-        event.headers['Authorization'] = event.headers['authorization'];
-      if( event.headers['user-agent'] )
-        event.headers['User-Agent'] = event.headers['user-agent'];
+    try{
+        var event;
+        var func;
+        if( func_table.hasOwnProperty(operationId) ){
+            event = {
+                headers: req.headers,
+                body: JSON.stringify(req.body),
+                path: req.swagger.apiPath,
+                httpMethod: req.method,
+                queryStringParameters: req.query,
+                requestContext: ( req.requestContext ) ? req.requestContext : {}
+            };
 
-      func = func_table[operationId];
-      res.func_type = "normal";
-    }else if( alexa_table.hasOwnProperty(operationId) ){
-      event = req.body;
+            event.Host = req.hostname;
 
-      func = alexa_table[operationId];
-      res.func_type = "alexa";
-    }else if( lambda_table.hasOwnProperty(operationId) ){
-      event = req.body.event;
+            func = func_table[operationId];
+            res.func_type = "normal";
+        }else if( alexa_table.hasOwnProperty(operationId) ){
+            event = req.body;
 
-      func = lambda_table[operationId];
-      res.func_type = "lambda";
-    }else{
-      console.log('can not found operationId: ' + operationId);
-      return_error(res, new Error('can not found operationId'));
-      return;
-    }
-    //  console.log(event);
+            func = alexa_table[operationId];
+            res.func_type = "alexa";
+        }else if( lambda_table.hasOwnProperty(operationId) ){
+            event = req.body.event;
 
-    var context = {
-      succeed: (msg)=>{
-        console.log('succeed called');
-        return_response(res, msg);
-      },
-      fail: (error) => {
-        console.log('failed called');
-        return_error(res, error);
-      }
-    };
-
-    var task = func(event, context, (error, response) =>{
-      console.log('callback called');
-      if( error )
-        return_error(res, error);
-      else
-        return_response(res, response);
-    });
-    if( task === undefined ){
-      return;
-    }else if( task instanceof Promise ){
-      task.then(ret =>{
-        if( ret ){
-          console.log('promise is called');
-          return_response(res, ret);
+            func = lambda_table[operationId];
+            res.func_type = "lambda";
+        }else{
+            console.log('can not found operationId: ' + operationId);
+            return_error(res, new Error('can not found operationId'));
+            return;
         }
-      });
-    }else{
-      console.log('return called');
-      return_response(res, task);
+        res.returned = false;
+
+//        console.log(event);
+
+        var context = {
+            succeed: (msg) => {
+                console.log('succeed called');
+                return_response(res, msg);
+            },
+            fail: (error) => {
+                console.log('failed called');
+                return_error(res, error);
+            },
+            original_req: req
+        };
+
+        var task = func(event, context, (error, response) =>{
+            console.log('callback called');
+            if( error )
+                return_error(res, error);
+            else
+                return_response(res, response);
+        });
+        if( task instanceof Promise || (task && typeof task.then === 'function') ){
+            task.then(ret =>{
+                if( ret ){
+                    console.log('promise is called');
+                    return_response(res, ret);
+                }else{
+                    console.log('promise return undefined');
+                    return_none(res);
+                }
+            })
+            .catch(err =>{
+                console.log('error throwed: ' + err);
+                return_error(res, err);
+            });
+        }else{
+            console.log('return called');
+            return_none(res);
+        }
+    }catch(err){
+        console.log('error throwed: ' + err);
+        return_error(res, err);
     }
-  }catch(err){
-    console.log('error throwed');
-    return_error(res, err);
-  }
+}
+
+function return_none(res){
+    if( res.returned )
+        return;
+    else
+        res.returned = true;
+
+    res.statusCode = 200;
+    res.type('application/json');
+
+    if( res.func_type == 'alexa' ){
+        res.json({});
+    }else if(res.func_type == 'lambda'){
+        res.json({ body: null });
+    }else{
+        res.json({});
+    }
 }
 
 function return_error(res, err){
-  res.status(500);
-  res.json({ errorMessage: err.toString() });
+    if( res.returned )
+        return;
+    else
+        res.returned = true;
+
+    res.status(500);
+    res.json({ errorMessage: err.toString() });
 }
 
 function return_response(res, ret){
-  if( ret.statusCode )
-    res.status(ret.statusCode);
-  for( var key in ret.headers )
-    res.set(key, ret.headers[key]);
+    if( res.returned )
+        return;
+    else
+        res.returned = true;
 
-//  console.log(ret.body);
+    if( ret.statusCode )
+        res.status(ret.statusCode);
+    for( var key in ret.headers )
+        res.set(key, ret.headers[key]);
 
-  if (!res.get('Content-Type'))
-    res.type('application/json');
+//    console.log(ret.body);
 
-  if( binary_table.indexOf(res.get('Content-Type')) >= 0 ){
-    var bin = new Buffer(ret.body, 'base64')
-    res.send(bin);
-  }else{
-    if( res.func_type == 'alexa' || res.func_type == 'lambda'){
-      res.send(JSON.stringify(ret));
+    if (!res.get('Content-Type'))
+        res.type('application/json');
+
+    if( binary_table.indexOf(res.get('Content-Type')) >= 0 ){
+        var bin = new Buffer(ret.body, 'base64')
+        res.send(bin);
     }else{
-      if( ret.body )
-        res.send(ret.body);
+        if( res.func_type == 'alexa'){
+            res.json(ret);
+        }else if( res.func_type == 'lambda'){
+            res.json({ body: ret });
+        }else{
+            if( ret.body )
+                res.send(ret.body);
+            else
+                res.json({});
+        }
     }
-  }
 }
